dfc564e8aeb0c90feaaa6d717edf5852f486e0ae
jalal <jalalmysk@gmail.com>
2025-06-26 02:43:52 +0100

    Second Commit

---
+++ lib/test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class justTesting extends StatelessWidget {
+  const justTesting({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
